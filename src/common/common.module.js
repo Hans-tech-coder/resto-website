6207c4fe8a5cc7f4c9293cc312605cefee23d39d
Hans-tech-coder <obenahans@gmail.com>
2022-12-09 22:51:31 +0800

    Coding Up  Interceptor

--- src/common/common.module.js
+++ src/common/common.module.js
@@ -1,6 +1,12 @@
 (function() {
-"use strict";
-
-angular.module('common', []);
-
-})();
+  "use strict";
+  
+  angular.module('common', [])
+  .config(config);
+  
+  config.$inject = ['$httpProvider'];
+  function config($httpProvider) {
+    $httpProvider.interceptors.push('loadingHttpInterceptor');
+  }
+  
+  })();
